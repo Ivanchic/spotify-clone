f6c6c5f41b60db64ced728e540952657f9e40d22
Ivanchik <iradolgaya88@icloud.com>
2025-07-23 22:05:20 +0400

    fixed img

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -17,7 +17,7 @@ const Topbar = () => {
     "
     >
       <div className="flex gap-2 items-center">
-        <img src="./public/spotify.png" className="size-8" alt="Spotify logo" />
+        <img src="/spotify.png" className="size-8" alt="Spotify logo" />
         Spotify
       </div>
       <div className="flex items-center gap-4">
